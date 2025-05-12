c59d4855b7653ee25621ee164dd2fb16ea6d07d1
Keg7691 <K.Growiec@web.de>
2025-05-12 23:17:25 +0200

    Add files via upload
    Aktuelles M-File enthält nur noch die benötigten Eingangsparameter für die Rover-Simulation. 
    Darin enthalten: Roverlänge, Roverbreite, Roverhöhe, Radradius und die Position der Räder.

--- Initialisierung_Rover_.m
+++ Initialisierung_Rover_.m
@@ -15,60 +15,18 @@ wheelPos = [ roverLength/2,  roverWidth/2;   % vorne links
             -roverLength/2,  roverWidth/2;   % hinten links
             -roverLength/2, -roverWidth/2];  % hinten rechts
 
-%% --- Parameter zur Steuerung des Rovers --
+%% --- Test-Startparameter zur Steuerung des Rovers ---
 
-% Startposition
-pose = [0; 0; 0];  % [x; y; theta(rad)]
+% Simulationszeit
+simTime = 10;
 
-% Für jedes Rad: [v_fl; v_fr; v_rl; v_rr] % lineare Geschwindigkeit [m/s]
-wheelSpeeds = [1; 1; 1; 1];
+% Startposition im Welt-Ksys [x; y; theta(rad)]
+q = [0; 0; 0];  
+
+% Für jedes Rad: [w_fl; w_fr; w_rl; w_rr] in [rad/s]
+dthetas = [1.0; 1.0; 1.0; 1.0];
 
 % Lenkwinkel in rad
 % vorne links, vorne rechts, h links..
-wheelSteerAng = [pi/8; pi/8; 0; 0];
+phi = [0.0; 0.0; 0.0; 0.0];
 
-% bis hier hin können Parameter auf jeden fall bleiben
-%% --- Matrix A gemäß Formel ---
-
-% 3 x 4 Matrix aus nullen
-A = zeros(3, 4);  % [vx; vy; omega]
-
-% for-Schleife 1-4 (für jedes Rad)
-for i = 1:4
-    alpha = wheelSteerAng(i);
-
-    % Zeilenvektor aus wheelPos (1 × n) = z.B. roverLength/2,  roverWidth/2
-    % (v.links)
-    r_i = wheelPos(i, :).';  % Radposition als Spaltenvektor
-
-    % r_i
-
-    % Beiträge zur Geschwindigkeit
-    A(1, i) = cos(alpha) / 4; % erste Zeile füllen
-    A(2, i) = sin(alpha) / 4;
-
-    % Beitrag zur Rotation
-    Wi = (-r_i(2)*cos(alpha) + r_i(1)*sin(alpha)) / ((4*r_i(1))^2 + (4*r_i(2)^2));
-    % Wi = r_i(1)*sin(alpha) - r_i(2)*cos(alpha);
-    A(3, i) = Wi;
-end
-
-%% --- Berechnung der Resultierenden Bewegung ---
-% 3 x 1
-v_rover = A * wheelSpeeds;
-
-vx = v_rover(1);
-vy = v_rover(2);
-omega = v_rover(3);
-
-%% --- Ausgabe ---
-disp('Geschwindigkeiten des Rovers:');
-fprintf('vx     = %.4f m/s\n', vx);
-fprintf('vy     = %.4f m/s\n', vy);
-fprintf('omega  = %.4f rad/s\n', omega);
-
-
-%% Ausgeben der Pose in x,y,theta & der Geschwindigkeit
-% 
-% disp('Letzte Pose:');
-% disp(pose);  % [x; y; theta]
