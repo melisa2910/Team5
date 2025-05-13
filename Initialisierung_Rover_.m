8966a98e96da19751fa3255de582c5dbc764a5b8
Keg7691 <K.Growiec@web.de>
2025-05-13 23:10:26 +0200

    Add files via upload
    M-File noch einmal bearbeitet - folgende Parameter sind (nur) noch enthalten: roverLength, roverWidth, roverHeight, wheelRadius, simTime, wheelPos
    Im vorherigen M-File waren noch Testparameter wie wheelSpeeds und wheelSteerAng enthalten. Diese habe ich entfernt.

--- Initialisierung_Rover_.m
+++ Initialisierung_Rover_.m
@@ -3,30 +3,17 @@ clear;
 clc; 
 close all;
 
-%% --- Roverparameter ---
+%% Roverparameter
 roverLength = 0.5;  % [m]
 roverWidth  = 0.4;  % [m]
 roverHeight = 0.3;  % [m]
 wheelRadius = 0.1;  % [m]
 
-% Rad-Positionen (Rover-Mitte = Bezugspunkt)
-wheelPos = [ roverLength/2,  roverWidth/2;   % vorne links
-             roverLength/2, -roverWidth/2;   % vorne rechts
-            -roverLength/2,  roverWidth/2;   % hinten links
-            -roverLength/2, -roverWidth/2];  % hinten rechts
-
-%% --- Test-Startparameter zur Steuerung des Rovers ---
-
 % Simulationszeit
 simTime = 10;
 
-% Startposition im Welt-Ksys [x; y; theta(rad)]
-q = [0; 0; 0];  
-
-% Für jedes Rad: [w_fl; w_fr; w_rl; w_rr] in [rad/s]
-dthetas = [1.0; 1.0; 1.0; 1.0];
-
-% Lenkwinkel in rad
-% vorne links, vorne rechts, h links..
-phi = [0.0; 0.0; 0.0; 0.0];
-
+% Rad-Positionen (Rover-Mitte = Bezugspunkt)
+wheelPos = [ roverLength/2, -roverWidth/2;   % vorne rechts
+             roverLength/2,  roverWidth/2;   % vorne links
+            -roverLength/2,  roverWidth/2;   % hinten links
+            -roverLength/2, -roverWidth/2];  % hinten rechts
