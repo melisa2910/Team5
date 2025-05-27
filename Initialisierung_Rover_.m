0b7ec00d148b6aaee0003e07c3590cf0aeba5312
Keg7691 <K.Growiec@web.de>
2025-05-27 22:02:37 +0200

    Erweiterung für Pfadplannung
    Vorbereitungen zur Implementierung der Pfadplannung.
    Im Initialisierungsfile für den Rover wurde eine Rastermap hinzugefügt, die nachher zur Pfadplannung benötigt wird.
    Des weiteren gibt es eine neue Funktion (fct_markCirularObstacle). Diese dient der Eintragung der Hindernisse, in die neue Rastermap.

--- Initialisierung_Rover_.m
+++ Initialisierung_Rover_.m
@@ -3,17 +3,87 @@ clear;
 clc; 
 close all;
 
-%% Roverparameter
-roverLength = 0.5;  % [m]
-roverWidth  = 0.4;  % [m]
-roverHeight = 0.3;  % [m]
-wheelRadius = 0.1;  % [m]
+%% Initialisierung Roverparameter
 
 % Simulationszeit
 simTime = 10;
 
-% Rad-Positionen (Rover-Mitte = Bezugspunkt)
-wheelPos = [ roverLength/2, -roverWidth/2;   % vorne rechts
-             roverLength/2,  roverWidth/2;   % vorne links
-            -roverLength/2,  roverWidth/2;   % hinten links
-            -roverLength/2, -roverWidth/2];  % hinten rechts
+l = 2;  % [m]       Länge Rover Körper
+b = 1;  % [m]       Breite Rover Körper
+h = 0.5;  % [m]       Höhe Rover Körper
+r = 0.2;  % [m]       Rad Radius
+w = 0.2;  % [m]       Breite der Räder
+
+z = 1;      % Höhe im Raum
+
+
+% Initialisierung Anfangszustände
+dtheta = ones(4,1);
+
+phi = ones(4,1);
+
+dq = zeros(3,1);
+
+q0 = zeros(3,1);
+
+
+%% Initialisierung der Umgebung und der Map
+
+% Erstelle ein Koordinatensystem im 3D-Raum
+figure;
+hold on;
+grid on;
+axis equal;
+
+% Variablen zum setzen der Größe (werden auch für Rastermap verwendet)
+xlim_end = 20;
+ylim_end = 20;
+
+xlim([0 xlim_end]);
+ylim([0 ylim_end]);
+zlim([0 5]);
+
+% Rastermap für Pfadplannung vorbereiten
+% [Zeilen, Spalten] der Matrix (entspricht 1m pro Zelle)
+mapSize = [ylim_end, xlim_end];
+map = zeros(mapSize);               % 0 = frei
+
+% Rover visualisieren
+fct_plotvehicle(1, 1, 1, 2, 1, 0.5, 0.2, 0.2);
+
+% Parameter und Funktionen für Hindernis 1
+
+X_value_obs1 = 6;
+Y_value_obs1 = 6;
+r1 = 1.4;
+
+fct_plotobstacle(X_value_obs1, Y_value_obs1, 0, r1, 1.5);
+% Eintragen der Hindernisse in Rastermap
+map = fct_markCircularObstacle(map, X_value_obs1, Y_value_obs1, r1);
+
+% Hindernis 2
+
+X_value_obs2 = 11;
+Y_value_obs2 = 15;
+r2 = 1.5;
+
+fct_plotobstacle(X_value_obs2, Y_value_obs2, 0, r2, 2);
+map = fct_markCircularObstacle(map, X_value_obs2, Y_value_obs2, r2);
+
+% Hindernis 3
+
+X_value_obs3 = 17;
+Y_value_obs3 = 8;
+r3 = 1.3;
+
+fct_plotobstacle(X_value_obs3, Y_value_obs3, 0, r3, 1);
+map = fct_markCircularObstacle(map, X_value_obs3, Y_value_obs3, r3);
+
+% Start- und Endpunkt
+% Evtl. Belegung von Start und Ziel auf einem Hindernis mit if-Abfrage
+% absichern
+
+start_pos = [1, 1, 0];
+end_pos   = [15, 15, 0];
+fct_plot_start_end(start_pos, end_pos);
+
---
+++ fct_markCircularObstacle.m
@@ -0,0 +1,22 @@
+function map = fct_markCircularObstacle(map, x_center, y_center, radius)
+% Funktion zur Eintragung von Hindernissen in die Rastermap
+% Rastermap wird für Pfadplannung benötigt
+
+    [rows, cols] = size(map);
+
+    % Schleife über alle Zellen
+    for x = 1:cols
+        for y = 1:rows
+            % Mittelpunkt der Zelle (1x1 Zellen)
+            cell_x = x - 0.5;
+            cell_y = y - 0.5;
+
+            % euklidischer Abstand Zellenmittelpunkt - Hindernismittelpunkt
+            dist = sqrt((cell_x - x_center)^2 + (cell_y - y_center)^2);
+
+            if dist <= radius
+                map(y, x) = 1;
+            end
+        end
+    end
+end
