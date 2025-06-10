d0eae02adb26c203c1d036f8657a270d2ed70dd0
Steven-h2303 <steven.hoppe@icloud.com>
2025-06-10 11:52:34 +0200

    ...

--- Initialisierung_Rover_.m
+++ Initialisierung_Rover_.m
@@ -8,8 +8,8 @@ close all;
 % Simulationszeit
 simTime = 10;
 
-l = 2;  % [m]       Länge Rover Körper
-b = 1;  % [m]       Breite Rover Körper
+l = 2;  % [m]         Länge Rover Körper
+b = 1;  % [m]         Breite Rover Körper
 h = 0.5;  % [m]       Höhe Rover Körper
 r = 0.2;  % [m]       Rad Radius
 w = 0.2;  % [m]       Breite der Räder
@@ -61,7 +61,7 @@ safety = b / 2;
 % absichern
 
 start_pos = [1, 1, 0];
-end_pos   = [5, 15, 0];
+end_pos   = [8, 10, 0];
 
 %% Rover visualisieren
 fct_plotvehicle(1, 1, 1, 2, 1, 0.5, 0.2, 0.2);
